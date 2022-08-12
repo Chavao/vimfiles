91790449fc3225b80b33f44d10bf2c9a618d10da
Diego Chavão <diego.chavao@corp.globo.com>
2022-08-12 16:20:35 -0300

    Add config for neovim on installation

--- install.sh
+++ install.sh
@@ -3,9 +3,21 @@ then
   mv -f ~/.vimrc ~/.vimrc-before-chavao-vimfiles
 fi
 
+if [ -e ~/.config/nvim/init.vim ]
+then
+  mv -f ~/.config/nvim/init.vim ~/.config/nvim/init.vim-before-chavao-vimfiles
+fi
+
 touch ~/.vimrc
 
 echo "source ~/.vim/vimrc" > ~/.vimrc
+
+mkdir -p ~/.config/nvim
+touch ~/.config/nvim/init.vim
+
+echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after" > ~/.config/nvim/init.vim
+echo "let &packpath = &runtimepath" >> ~/.config/nvim/init.vim
+echo "source ~/.vimrc" >> ~/.config/nvim/init.vim
 
 if [ -e ~/.vim ]
 then
